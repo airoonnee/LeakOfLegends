7431e2d3c2283fbf1e366706249f4ce7a2fcb45c
airoonnee <erwanagesne@gmail.com>
2025-01-23 16:59:18 +0100

    fun all info of champion

--- src/Search.py
+++ src/Search.py
@@ -1,43 +1,44 @@
 import pandas as pd
 import keyboard  
 
-file_path = "data/test.csv"  
-try:
-    data = pd.read_csv(file_path, sep=";")
-except FileNotFoundError:
-    print(f"Le fichier {file_path} n'a pas été trouvé.")
-    exit()
+class Search:
+    file_path = "data/test.csv"  
+    try:
+        data = pd.read_csv(file_path, sep=";")
+    except FileNotFoundError:
+        print(f"Le fichier {file_path} n'a pas été trouvé.")
+        exit()
 
-if "Name" not in data.columns:
-    print("La colonne 'Name' n'est pas présente dans le fichier.")
-    exit()
+    if "Name" not in data.columns:
+        print("La colonne 'Name' n'est pas présente dans le fichier.")
+        exit()
 
-search_query = ""
+    search_query = ""
 
-def search_champion(query):
-    """Recherche les names correspondant à la requête."""
-    if query.strip() == "":  
-        print("\rListe complète des names :")
-        print(data['Name'].to_string(index=False))
-    else:
-        results = data[data['Name'].str.contains(query, case=False, na=False)]
-        if results.empty:
-            print(f"\rAucun name trouvé pour : '{query}'", end="")
+    def search_champion(query):
+        """Recherche les names correspondant à la requête."""
+        if query.strip() == "":  
+            print("\rListe complète des names :")
+            print(Search.data['Name'].to_string(index=False))
         else:
-            print(f"\n\rNames trouvés pour '{query}': {', '.join(results['Name'])}", end="")
+            results = Search.data[data['Name'].str.contains(query, case=False, na=False)]
+            if results.empty:
+                print(f"\rAucun name trouvé pour : '{query}'", end="")
+            else:
+                print(f"\n\rNames trouvés pour '{query}': {', '.join(results['Name'])}", end="")
 
-def on_key_event(e):
-    """Déclenche la recherche à chaque frappe de touche."""
-    global search_query
-    if e.name == "backspace":
-        search_query = search_query[:-1]  
-    elif e.name == "enter":
-        print("\nRecherche terminée.")
-        keyboard.unhook_all()  
-    elif len(e.name) == 1:  
-        search_query += e.name
-    search_champion(search_query)
+    def on_key_event(e):
+        """Déclenche la recherche à chaque frappe de touche."""
+        global search_query
+        if e.name == "backspace":
+            search_query = search_query[:-1]  
+        elif e.name == "enter":
+            print("\nRecherche terminée.")
+            keyboard.unhook_all()  
+        elif len(e.name) == 1:  
+            search_query += e.name
+        Search.search_champion(search_query)
 
-print("Tapez au fur et à mesure pour rechercher un name (Appuyez sur 'Entrée' pour quitter) :")
-keyboard.on_press(on_key_event)  
-keyboard.wait("enter")  
+    print("Tapez au fur et à mesure pour rechercher un name (Appuyez sur 'Entrée' pour quitter) :")
+    keyboard.on_press(on_key_event)  
+    keyboard.wait("enter")  
--- src/Top.py
+++ src/Top.py
@@ -1,32 +1,31 @@
 import pandas as pd
 
 file_path = "data/test.csv"  
-df =  pd.read_csv(file_path, sep=";")
+# df =  pd.read_csv(file_path, sep=";")
 output_file = "data/top_results.csv"
 
 
-top_Win = df[['Name', 'Win %']].sort_values(by='Win %', ascending=False).head(10)
-with open(output_file, mode="w", encoding="utf-8") as f:
-    top_Win.to_csv(f, index=False, sep=";", lineterminator="\n")
-print(top_Win)
+class Top:
+    def top_win(df, nbr):
+        top_Win = df[['Name', 'Win %']].sort_values(by='Win %', ascending=False).head(nbr)
+        with open(output_file, mode="w", encoding="utf-8") as f:
+            top_Win.to_csv(f, index=False, sep=";", lineterminator="\n")
+        print(top_Win)
 
-output_file = "data/top_results2.csv"
+    def top_pick(df, nbr):
+        top_Pick = df[['Name', 'Pick %']].sort_values(by='Pick %', ascending=False).head(nbr)
+        with open(output_file, mode="w", encoding="utf-8") as f:
+            top_Pick.to_csv(f, index=False, sep=";", lineterminator="\n")
+        print(top_Pick)
 
-top_Pick = df[['Name', 'Pick %']].sort_values(by='Pick %', ascending=False).head(10)
-with open(output_file, mode="w", encoding="utf-8") as f:
-    top_Pick.to_csv(f, index=False, sep=";", lineterminator="\n")
-print(top_Pick)
+    def top_ban(df, nbr):
+        top_Ban = df[['Name', 'Ban %']].sort_values(by='Ban %', ascending=False).head(nbr)
+        with open(output_file, mode="w", encoding="utf-8") as f:
+            top_Ban.to_csv(f, index=False, sep=";", lineterminator="\n")
+        print(top_Ban)
 
-output_file = "data/top_results3.csv"
-
-top_Ban = df[['Name', 'Ban %']].sort_values(by='Ban %', ascending=False).head(10)
-with open(output_file, mode="w", encoding="utf-8") as f:
-    top_Ban.to_csv(f, index=False, sep=";", lineterminator="\n")
-print(top_Ban)
-
-output_file = "data/top_results4.csv"
-
-top_KDA = df[['Name', 'KDA']].sort_values(by='KDA', ascending=False).head(10)
-with open(output_file, mode="w", encoding="utf-8") as f:
-    top_KDA.to_csv(f, index=False, sep=";", lineterminator="\n")
-print(top_KDA)
+    def top_kda(df, nbr):
+        top_KDA = df[['Name', 'KDA']].sort_values(by='KDA', ascending=False).head(nbr)
+        with open(output_file, mode="w", encoding="utf-8") as f:
+            top_KDA.to_csv(f, index=False, sep=";", lineterminator="\n")
+        print(top_KDA)
