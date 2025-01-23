af163fee3812c68b88e4fc5f3ae70034c03a5733
Kilian <kilian.moun@ynov.com>
2025-01-23 21:12:41 +0100

    fix database

--- src/utils/databaseRawToInterim.py
+++ src/utils/databaseRawToInterim.py
@@ -2,11 +2,15 @@ import os
 import json
 import pandas as pd
 
+BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data')
+DATA_DIR = os.path.abspath(BASE_DIR)
+
 def process_file(file_path, columns_to_keep=None):
+    input_path = os.path.join(DATA_DIR, 'raw', file_path)
     file_extension = file_path.split('.')[-1].lower()
     
     if file_extension == 'json':
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(input_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
         if 'data' in data:
             champions_data = data['data']
@@ -37,7 +41,7 @@ def process_file(file_path, columns_to_keep=None):
                 df = pd.concat([df, passive_df], axis=1)
 
     elif file_extension == 'csv':
-        df = pd.read_csv(file_path, delimiter=';')
+        df = pd.read_csv(input_path, delimiter=';')
     
     if columns_to_keep is not None:
         missing_columns = [col for col in columns_to_keep if col not in df.columns]
@@ -53,8 +57,8 @@ def process_file(file_path, columns_to_keep=None):
 
 
 if __name__ == "__main__":
-    json_file_path = "data/raw/championFull.json"
-    csv_file_path = "data/raw/lolStats.csv"
+    json_file_path = "championFull.json"
+    csv_file_path = "lolStats.csv"
     
     columns_to_keep_csv = ["Name", "Class", "Role", "Tier", "Win %", "Pick %", "Ban %", "KDA"] 
     columns_to_keep_json = ['name', 'title', 'lore', 'spell.0.id', 'spell.0.name', 'spell.0.description', 'spell.1.id', 'spell.1.name', 'spell.1.description', 'spell.2.id', 'spell.2.name', 'spell.2.description', 'spell.3.id', 'spell.3.name', 'spell.3.description', "passive.name", "passive.description"] 
